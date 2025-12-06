9e58710f3e779ac63f516e3c53c4d6cc4e78255a
Checkmate-333 <kuntal.biswas.0698@gmail.com>
2025-12-06 12:14:06 +0530

    Add QR code generation functionality

--- QRgenerator.py
+++ QRgenerator.py
@@ -1,1 +1,13 @@
+import qrcode
 
+url = input ("Enter the URL to generate QR code: ").strip()
+
+file_path = "C:\\Users\\biswa\\Desktop\\qrcode.png"
+
+qr = qrcode.QRCode()
+qr.add_data(url)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save(file_path)
+
+print(f"QR code generated and saved to {file_path}")
